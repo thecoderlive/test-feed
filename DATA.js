0090e7e2c4c24c6156b815cd2f963c8eb39cb822
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2022-12-26 12:06:41 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "44917a0e-06ef-4481-a745-933255c3f3bd",
+    "home_feed": [
+        {
+            "id": "7cafb433-cb4e-425f-8eab-1e8eb88ecd9c",
+            "artist_name": "simply",
+            "home_carousel": [
+                {
+                    "id": "1d25a331-846f-41ea-9f70-791f1be1f693",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "755dfce5-9881-47ba-9deb-daf6a7815939",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/103.png"
+                },
+                {
+                    "id": "7396fba4-7a7c-416a-85f5-8496646c7d4d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png"
+                },
+                {
+                    "id": "411a6492-d907-4855-9850-1b13ea48989f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                },
+                {
+                    "id": "2cd05912-eb9d-497d-9176-61ee8bf38f83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                }
+            ],
+            "art_caption": "close",
+            "art_description": "Sea price her action network. Save where movie stage."
+        },
+        {
+            "id": "a8ba076e-ab81-4ae5-967c-9da6661cb126",
+            "artist_name": "enjoy",
+            "home_carousel": [
+                {
+                    "id": "1db09670-5808-43c5-946c-3296803280ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                },
+                {
+                    "id": "0f2c8069-e342-4a5a-b579-f57ea33b8b42",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "a3008a25-c278-448f-872d-5642002440b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png"
+                },
+                {
+                    "id": "ca58fccd-01ec-475a-a7d6-18832b5a61a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png"
+                },
+                {
+                    "id": "1e54ae24-6598-4366-a94e-8c787c3c7bf5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png"
+                }
+            ],
+            "art_caption": "eight",
+            "art_description": "Eye spend PM age. Adult culture scientist."
+        },
+        {
+            "id": "46fd0164-a6cc-4b3d-aca7-fc3ee875a24f",
+            "artist_name": "sense",
+            "home_carousel": [
+                {
+                    "id": "53aea9c6-fdcc-430d-8479-657d5bb5f948",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "f9bdd83f-e199-457f-9c8b-46284660a5a2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/147.png"
+                },
+                {
+                    "id": "88ba69bc-c214-4545-aba1-64323bc99a29",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "9070b6e8-a096-46a7-aa11-870aaca7388d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "80227218-9b9b-43e2-b6ac-2169961ba1fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                }
+            ],
+            "art_caption": "dog",
+            "art_description": "Poor break ask summer arm anyone. Nice property politics manager painting grow every rest."
+        },
+        {
+            "id": "a17e1deb-c4b6-4341-9510-77ed0111e21d",
+            "artist_name": "agent",
+            "home_carousel": [
+                {
+                    "id": "455f9742-a26a-4798-ac7b-a13eb4364356",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "40117a8e-f5df-4a26-bdab-6259112fbd62",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "bf9e3a89-ecd4-4581-8c1a-1803e1218fa8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                },
+                {
+                    "id": "20fc7381-6900-4830-865a-bd5f63ab8644",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "5179288c-27ce-4d00-a3db-d4a088a748e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                }
+            ],
+            "art_caption": "special",
+            "art_description": "Test find story. Could other yet middle tree wife own population."
+        },
+        {
+            "id": "ea0a592f-a5ef-419a-9db9-eb3b25e2f7d3",
+            "artist_name": "collection",
+            "home_carousel": [
+                {
+                    "id": "136e4874-ad8f-41e4-a265-18a34a4bf8b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/151.png"
+                },
+                {
+                    "id": "a7ffccc1-8005-4fe9-a348-b87218cb9a1b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "7409ccc3-b405-4444-ab63-a41781eb13ef",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png"
+                },
+                {
+                    "id": "4a7f5700-7092-4776-adad-a0a7fdaa7c0f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "23e7e124-0e1f-41ed-a258-6944d872a7b1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                }
+            ],
+            "art_caption": "increase",
+            "art_description": "Century sport radio rule statement environment learn charge. Recognize hand chance finally return example local. Player cell each consider. East behind hold senior they enter."
+        }
+    ]
+}
